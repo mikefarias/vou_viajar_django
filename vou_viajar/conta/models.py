1226a13d3902cb86ffd90158e49307d351504654
Carlson Santana Cruz <carlsonsantana@gmail.com>
2019-01-29 01:01:41 -0300

    :bug: Utiliza os campos corretos para os modelos

--- vou_viajar/conta/models.py
+++ vou_viajar/conta/models.py
@@ -3,15 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Agencia(models.Model):
-    cnpj_cpf = models.CharField(max_length=14, null=False, blank=False)
-    nome_juridico = models.TextField(max_length=30, null=False, blank=False)
-    nome_fantasia = models.TextField(max_length=30, null=False, blank=False)
     cod_cadastur = models.CharField(max_length=10, null=False, blank=False) #quantos digitos ??
-    nome_responsavel = models.TextField(max_length=30, null=False, blank=False)
-    cpf_responsavel = models.TextField(max_length=14)
+    cnpj_cpf = models.CharField(max_length=14, null=False, blank=False)
+    nome_juridico = models.CharField(max_length=100, null=False, blank=False)
+    nome_fantasia = models.CharField(max_length=100, null=False, blank=False)
+    nome_responsavel = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+    )
+    cpf_responsavel = models.CharField(max_length=14)
     agencia_fisica = models.BooleanField(null=False, blank=False)
     foto_perfil = models.ImageField(null=True, blank=True)
-    endereco = models.TextField(max_length=30)
+    endereco = models.CharField(max_length=200)
     email = models.EmailField(null=False, blank=False)
     telefone = models.CharField(max_length=11)
     fan_page_oficial = models.URLField(null=True, blank=True)
