0e0dc35a20572159f67e3a46a221542f7810b3ba
mike <mike@shoppingonlinearacaju.com.br>
2020-04-21 08:15:36 -0300

    :sparkles: Adicionando models de conta no admin do django

--- vou_viajar/conta/admin.py
+++ vou_viajar/conta/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, TravelAgency, Profile, ContactTravelAgency
+
+admin.site.register(User)
+admin.site.register(TravelAgency)
+admin.site.register(Profile)
+admin.site.register(ContactTravelAgency)
